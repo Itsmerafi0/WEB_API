b854c10f29d5bcaa68b108b4665c287b5b80fba6
Rafi Azhar <itsmerafi0@gmail.com>
2023-05-17 10:27:13 +0700

    Initial commit

---
+++ API/Models/AccountRole.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Models
+{
+    public class AccountRole
+    {
+        public Guid Guid { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModefiedDate { get; set; }
+        public Guid AccountGuid { get; set; }
+        public Guid RoleGuid { get; set; }
+
+    }
+}
---
+++ API/Models/University.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Models
+{
+    public class University
+    {
+        public Guid Guid { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
